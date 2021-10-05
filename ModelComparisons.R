fc71a6c1d5a4dcca1e681dd081e4342574032e66
Fernando Vazquez <51683809+fernando-vazquez3@users.noreply.github.com>
2021-10-05 13:39:34 -0400

    Update ModelComparisons.R

--- ModelComparisons.R
+++ ModelComparisons.R
@@ -1,11 +1,10 @@
 
 library(tidyverse)
-library(randomForest)
-library(psych)
-library(PRROC)
-library(caret)
-library(e1071)
 library(smotefamily)
+library(devtools)
+
+("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/ModelComparisons.R")
+
 
 #Version de prueba para el grupo
 ACE =tibble(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/ACE.csv"))
@@ -16,13 +15,6 @@ RFE_NB =tibble(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTP
 RFE_CF =tibble(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/RFE_CF.csv"))
 
 #Folds
-#devtools::install_github("CCMH-PSU/CCMHr")
-
-#F1 = as.character(loadRDa("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/F1.rda"))
-#F2 = c(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/F2.csv"))
-#F3 = c(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/F3.csv"))
-#F4 = c(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/F4.csv"))
-#F5 = c(read.csv("https://raw.githubusercontent.com/fernando-vazquez3/RTPC-4-year-assessment/master/F5.csv"))
 
 F1= c(64, 85, 91, 72, 83, 98, 116, 67, 87, 73, 76, 57, 6, 39, 50, 22, 3, 42, 55, 32, 49, 26, 44)
 F2= c(100, 82, 113, 90, 61, 79, 105, 86, 77, 96, 102, 56, 35, 5, 30, 38, 19, 59, 13, 41, 8, 58, 45)
@@ -31,74 +23,46 @@ F4= c(92, 80, 109, 112, 62, 111, 103, 88, 81, 108, 104, 7, 46, 11, 28, 10, 31, 4
 F5= c(1, 2, 12, 15, 20, 25, 29, 33, 34, 36, 53, 60, 63, 65, 66, 68, 71, 74, 78, 95, 101, 107, 110, 114)
 
 Folds=list(F1,F2,F3,F4,F5)
+names(Folds)=c("F1","F2","F3","F4","F5")
 numeric=c(5,0,3,0,0,5)
+names(numeric)=c("ACE","CFS","GAFS","RFE_RF","RFE_NB","RFE_CF")
 
 Datasets=list(ACE,CFS,GAFS,RFE_RF,RFE_NB,RFE_CF)
 names(Datasets)=c("ACE","CFS","GAFS","RFE_RF","RFE_NB","RFE_CF")
 
-for (i in 1:6){
-data=Datasets[[i]]
-data[,1:ncol(data)]<- lapply(data[,1:ncol(data)], as.numeric)
-table(data$Y)
-
-#SMOTe resampling data
-resample=SMOTE(X=data[,-ncol(data)],target=as.vector(data$Y),K=3,dup_size=1)
-table(resample$data$class)
-
-#Now we rebuild the data set which is ready
-data=resample$data
-Y = c(data$class)
-X = data.frame(data[1:ncol(data)-1])
-data=cbind(X,Y)
-data[,ncol(data)]=as.character(data[,ncol(data)])
-
-if(numeric[i]>0){
-  data[,1:numeric[i]]<- lapply(data[, 1:numeric[i]], as.numeric)
-  data[,(numeric[i]+1):(ncol(data)-1)]=round(data[,(numeric[i]+1):(ncol(data)-1)],4)
-  
-  data[,(numeric[i]+1):ncol(data)]<- lapply(data[, (numeric[i]+1):ncol(data)], as.factor)
-  ycol=ncol(data)
-  names(data[ycol])="Y"
-}
-else{
-  data[,(numeric[i]+1):ncol(data)]<- lapply(data[, (numeric[i]+1):ncol(data)], as.factor)
-  ycol=ncol(data)
-  names(data[ycol])="Y"
-}
-}
 
 glimpse(data)
 
 nFolds=5
 #set.seed=()
 
-#Assing rows per class
-permClass=list()
-obsC=list()
-p=table(data$Y)/nrow(data)
-for (i in 1:length(levels(data$Y)))
-{
-  selectedRows=as.numeric(row.names(data[data$Y==levels(data$Y)[i],]))
-  permRows=sample(x=selectedRows,size=length(selectedRows),replace=FALSE)
-  assign(paste("permClass",i,sep=""),permRows)
-  permClass[[i]]=permRows
-  assign(paste("obsC",i,sep=""),p[[i]][1]*floor(nrow(data)/nFolds))
-  obsC[[i]]=round(p[[i]][1]*(nrow(data)/nFolds))
-}
-
-#Create folds for training and testing
-rowsFold=list()
-for(i in 1:(nFolds-1)){
-  rows=list()
-  for (j in 1:length(levels(data$Y))){
-    if ((i-1)==0){rows[[j]] = permClass[[j]][c(i:(i*obsC[[j]]))]}
-    else{rows[[j]] = permClass[[j]][((i-1)*obsC[[j]]+1):(i*obsC[[j]])]}
-  }
-  name=paste("F",i,sep="")
-  rowsFold[[name]]=unlist(rows)
-}
-remaining = setdiff(1:nrow(data),as.numeric(unlist(rowsFold)))
-rowsFold[[paste("F",nFolds,sep="")]]=remaining
+# #Assing rows per class
+# permClass=list()
+# obsC=list()
+# p=table(data$Y)/nrow(data)
+# for (i in 1:length(levels(data$Y)))
+# {
+#   selectedRows=as.numeric(row.names(data[data$Y==levels(data$Y)[i],]))
+#   permRows=sample(x=selectedRows,size=length(selectedRows),replace=FALSE)
+#   assign(paste("permClass",i,sep=""),permRows)
+#   permClass[[i]]=permRows
+#   assign(paste("obsC",i,sep=""),p[[i]][1]*floor(nrow(data)/nFolds))
+#   obsC[[i]]=round(p[[i]][1]*(nrow(data)/nFolds))
+# }
+# 
+# #Create folds for training and testing
+# rowsFold=list()
+# for(i in 1:(nFolds-1)){
+#   rows=list()
+#   for (j in 1:length(levels(data$Y))){
+#     if ((i-1)==0){rows[[j]] = permClass[[j]][c(i:(i*obsC[[j]]))]}
+#     else{rows[[j]] = permClass[[j]][((i-1)*obsC[[j]]+1):(i*obsC[[j]])]}
+#   }
+#   name=paste("F",i,sep="")
+#   rowsFold[[name]]=unlist(rows)
+# }
+# remaining = setdiff(1:nrow(data),as.numeric(unlist(rowsFold)))
+# rowsFold[[paste("F",nFolds,sep="")]]=remaining
 
 metrics=c("Accuracy", "Kappa", "F1","Sensitivity","Specificity", "Pos Pred Value", "Neg Pred Value","Detection Rate", "Desirability")
 
@@ -125,19 +89,19 @@ for (Data in c("ACE","CFS","GAFS","RFE_RF","RFE_NB","RFE_CF")){
   data=cbind(X,Y)
   data[,ncol(data)]=as.character(data[,ncol(data)])
   
-  if(numeric[i]>0){
-    data[,1:numeric[i]]<- lapply(data[, 1:numeric[i]], as.numeric)
-    data[,(numeric[i]+1):(ncol(data)-1)]=round(data[,(numeric[i]+1):(ncol(data)-1)],4)
+  if(numeric[Data]>0){
+    data[,1:numeric[Data]]<- lapply(data[, 1:numeric[Data]], as.numeric)
+    data[,(numeric[Data]+1):(ncol(data)-1)]=round(data[,(numeric[Data]+1):(ncol(data)-1)],4)
     
-    data[,(numeric[i]+1):ncol(data)]<- lapply(data[, (numeric[i]+1):ncol(data)], as.factor)
+    data[,(numeric[Data]+1):ncol(data)]<- lapply(data[, (numeric[Data]+1):ncol(data)], as.factor)
     ycol=ncol(data)
     names(data[ycol])="Y"
   }
-  else{
-    data[,(numeric[i]+1):ncol(data)]<- lapply(data[, (numeric[i]+1):ncol(data)], as.factor)
+  
+    data[,(numeric[Data]+1):ncol(data)]<- lapply(data[, (numeric[Data]+1):ncol(data)], as.factor)
     ycol=ncol(data)
     names(data[ycol])="Y"
-  }
+  
   
 for (nTrees in seq(25,300,25))
 {
@@ -146,7 +110,7 @@ for (nTrees in seq(25,300,25))
     for (mTry in min:max)#min:max)
     {
       rF_mod=cvError.RF(ntree=nTrees,maxnodes=mNodes,mtry=mTry)
-      rfResults=rbind(rfResults, data.frame(Data,nTrees,mNodes,mTry,rF_mod$Mean,rF_mod$SE))
+      rfResults=rbind(rfResults, data.frame(nTrees,mNodes,mTry,rF_mod$Mean,rF_mod$SE))
       rfResultsComplete=rbind(rfResultsComplete,data.frame(nTrees,mNodes,mTry,t(rF_mod$meanPM),rF_mod$SE))
     }
   }
@@ -204,8 +168,8 @@ for (minSplit in 5:10)
       for (maxDepth in c(6,8,10))
       {
         rPART_mod=cvError.RPART(minsplit=minSplit, minbucket=minBucket,cp=Cp,maxdepth=maxDepth)
-        rpartResults=rbind(rpartResults, data.frame(minSplit,minBucket,Cp,maxDepth,rPART_mod$Mean,rPART_mod$SE))
-        rpartResultsComplete=rbind(rpartResultsComplete,data.frame(minSplit,minBucket,Cp,maxDepth,t(rPART_mod$meanPM),rPART_mod$SE))
+        rpartResults=rbind(rpartResults, data.frame(Data,minSplit,minBucket,Cp,maxDepth,rPART_mod$Mean,rPART_mod$SE))
+        rpartResultsComplete=rbind(rpartResultsComplete,data.frame(Data,minSplit,minBucket,Cp,maxDepth,t(rPART_mod$meanPM),rPART_mod$SE))
         
       }
     }
@@ -307,7 +271,7 @@ for (Type in c("C-classification","nu-classification")){
       for(Degree in 1:2) {
         SVM_mod=cvError.SVM(type=Type,kernel=Kernel,degree=Degree)
         SVMResults=rbind(SVMResults, c(Data,Type,Kernel,SVM_mod$Mean,SVM_mod$SE))
-        SVMResultsComplete=rbind(SVMResultsComplete, data.frame(Type,Kernel,SVM_mod$Performance.Measures[,1:4]))
+        SVMResultsComplete=rbind(SVMResultsComplete, data.frame(Data,Type,Kernel,SVM_mod$Performance.Measures[,1:4]))
         }
       }
     }
